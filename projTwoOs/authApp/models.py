8517fd87581d0e93403d97b5250cf0de97803a44
Andre Huang <huangand000@gmail.com>
2018-02-26 11:21:00 -0800

    Added database

--- projTwoOs/authApp/models.py
+++ projTwoOs/authApp/models.py
@@ -9,11 +9,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=100)
-<<<<<<< HEAD
-=======
     def check_password(self, raw_password):
         if self.password == raw_password:
             return True
         else:
             return False
->>>>>>> 80fb587b1e805b684a3915d0730670f8c45a2640
