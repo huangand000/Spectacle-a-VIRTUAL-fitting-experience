7220ca84f884e751cd523fac99df8a51409f20ea
Xiwen Li <mac@macdeMacBook-Air.local>
2018-02-26 15:24:54 -0800

    encrypted

--- projTwoOs/authApp/forms.py
+++ projTwoOs/authApp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from django.contrib.auth import get_user_model
 
+import bcrypt
+
 User = get_user_model()
 
 class RegistrationForm(forms.ModelForm):
@@ -13,9 +15,8 @@ class RegistrationForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super(RegistrationForm, self).clean()
-        print self.cleaned_data.get('password')
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
         if password != confirm_password:
             raise forms.ValidationError('password and confirm_password don\'t match.')
-        print self.cleaned_data.get('password')
+        self.cleaned_data['password'] = bcrypt.hashpw(self.cleaned_data.get('password').encode('utf8'), bcrypt.gensalt())
