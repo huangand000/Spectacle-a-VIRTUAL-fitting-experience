cc9d718e8fbf7a26b1003059fe3c397accb04197
Xiwen Li <mac@macdeMacBook-Air.local>
2018-02-26 18:58:32 -0800

    file upload saved in main directory

--- projTwoOs/dashboard_app/models.py
+++ projTwoOs/dashboard_app/models.py
@@ -9,3 +9,7 @@ class Glasses(models.Model):
     name = models.CharField(max_length=255)
     route = models.CharField(max_length=255)
     users = models.ManyToManyField(User, related_name='glasses')
+
+class Snapshot(models.Model):
+    file_upload = models.FileField(upload_to='static/snapshots')
+    user = models.ForeignKey(User, related_name='snapshots')
--- projTwoOs/dashboard_app/urls.py
+++ projTwoOs/dashboard_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^wishlist_process/$', views.wishlist_process),
     url(r'^webcam/$', views.webcam),
     url(r'^process/(?P<id>\d+)$', views.process),
-    url(r'^save_process/', views.save_process),
+    url(r'^save_snapshot$', views.save_snapshot),
     url(r'^get_glasses/', views.get_glasses),
 
 ]
