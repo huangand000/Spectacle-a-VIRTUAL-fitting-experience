d4efb0488565ca915e395390965fd43033fa7350
laurencesk <laurencesk@gmail.com>
2018-02-28 12:00:14 -0800

    adding chatbot, twitter

--- projTwoOs/chatbot/urls.py
+++ projTwoOs/chatbot/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^bot$', views.bot)
+    url(r'^bot$', views.bot),
+    url(r'^twitter$', views.twitter)
+
 ]
--- projTwoOs/chatbot/views.py
+++ projTwoOs/chatbot/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 from django.views.decorators.csrf import csrf_exempt 
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
-
+import datetime
 import random
+
+from twitter import *
+
+# import twitter
 # from speech import *
 
 
@@ -12,57 +16,97 @@ def index(request):
     return render(request,"chatbot/index.html")
 
 @csrf_exempt
-def bot(text):
-    return JsonResponse({'name':'laurece'})
+def bot(request):
+    userInput = str(request.POST['input'].lower())
+    CONVERSING = True
+    memory = []
+    response =''
 
-# CONVERSING = True
+    greetings = ['Hola', 'Hello', 'hi','hey!','Hello','Hi','Ni Hao']
+    
+    greetings_questions = ['how are you?','how are you doing?','how are you','how are you doing']
+    greetings_response= ['Okay','I am fine!','I am doing great!']
+    
+    having_questions = ['a question']
+    question_response = ['How can I help you?']
 
-# memory = []
-# greetings = ['hola', 'hello', 'hi','hey!','Hello','Hi']
-# questions = ['How are you?','How are you doing?']
-# responses = ['Okay','I am fine']
-# validations = ['yes','yeah','yea','no','No','Nah','nah']
-# verifications = ['Are you sure?','You sure?','you sure?','sure?',"Sure?"]
+    time_question = ['time is']
+    time_response = ['It is now '+datetime.datetime.now().strftime('%H:%M')]
 
-# storInfo = ['Please refer store locator for more store questions']
-# storeQuestions =['location','hours','stores','store','open','close','distance']
+    validations = ['yes','yeah','yea','no','No','Nah','nah']
+    verifications = ['Are you sure?','You sure?','you sure?','sure?',"Sure?"]
+    storInfo = ['Please refer store locator for more store information']
+    storeQuestions =['location','hours','stores','store','open','close','distance','address']
 
-# while CONVERSING:
-# 	lang = 'en-US'
-# 	speed = .3
-	
-# 	# userInput = text
-#   	userInput = raw_input("How can I help you?")
-# 	if userInput in greetings:
-# 		random_greeting = random.choice(greetings)
-# 		# say(random_greeting)
-# 		print random_greeting
-# 		memory.append((userInput,random_greeting))
-# 	elif any(word in (userInput).split() for word in storeQuestions):
-# 		response = storInfo
-# 		# say(response)
-# 		memory.append((userInput,response))
-# 		print response
-# 	elif userInput in verifications:
-# 		random_response = random.choice(validations)
-# 		# say(random_response)
-# 		memory.append((userInput,random_response))
-# 		print random_response
-# 	elif userInput in storeQuestions:
-# 		random_response = random.choice(validations)
-# 		# say(random_response)
-# 		memory.append((userInput,random_response))
-# 		print random_response
+    thanks = ['thank']
+    thanks_response = ['You are very welcome'] 
 
-# 	elif 'sure' in userInput:
-# 		response = "Sure about what?"
-# 		# say(response)
-# 		memory.append(('sure',response))
-# 		print response
-# 	else:
-# 		# say("I did not understand what you said. Goodbye")
 
-# 		CONVERSING = False
-		
-# for conversations in memory:
-# 	print conversations
+    while CONVERSING:
+        if  any(word in userInput for word in greetings):
+            random_greeting = random.choice(greetings)
+            # say(random_greeting)
+            response = random_greeting
+            CONVERSING = False
+        elif userInput in greetings_questions:
+            response = random.choice(greetings_response)
+            # say(response)
+            CONVERSING = False
+        elif any(word in userInput for word in having_questions):
+            response = question_response
+            # say(response)
+            CONVERSING = False
+        elif any(word in userInput for word in time_question):
+            response = time_response
+            # say(response)
+            CONVERSING = False
+        elif any(word in userInput for word in thanks):
+            response = thanks_response
+            # say(response)
+            CONVERSING = False
+        elif any(word in (userInput) for word in storeQuestions):
+            response = storInfo
+            # say(response)
+            CONVERSING = False
+        elif userInput in verifications:
+            random_response = random.choice(validations)
+            # say(random_response)
+            CONVERSING = False
+        elif userInput in storeQuestions:
+            random_response = random.choice(validations)
+            # say(random_response)
+            CONVERSING = False
+        elif 'sure' in userInput:
+            response = "Sure about what?"
+            CONVERSING = False
+            # say(response)
+        else:
+            response = "I did not understand your question"
+            # say(response)
+            CONVERSING = False
+            # CONVERSING = False
+            
+    context = {
+        'response': response
+    }
+    return JsonResponse(context)
+
+
+
+def twitter(request):
+
+    t = Twitter(
+        auth=OAuth('2990475133-m93dLsG6UoGtzm6aHSq89xmX1Z2Ysa8anGuCPU5', 'hk1cMw7EqwAVANozQNjMRVgqacoiGGQ2FVTaHmvsJjTuU', 'bnqpiVEnkqnOlfQFbxJHBJjoM', 'lcODwzLhRZV4OgHFxlaZphdT5IQGbas64ZsSiTCwbbpflazcJW')
+        )
+
+    tweets = t.search.tweets(q='#glasses trend', result_type='mixed', lang='en',include_entities='false')
+    
+    context = {
+        'tweets': tweets['statuses'] 
+    }
+    
+    # print tweets['statuses']
+    return render(request,'twitter.html',context)
+
+
+
